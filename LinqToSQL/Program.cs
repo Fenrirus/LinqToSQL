5710c31ca124711efcf77e08508e05876624307e
Marian Marian <fenrir55@gmail.com>
2018-11-03 21:50:40 +0100

    Pierwsze zapytanie

--- LinqToSQL/Program.cs
+++ LinqToSQL/Program.cs
@@ -10,6 +10,19 @@ namespace LinqToSQL
     {
         static void Main(string[] args)
         {
+            TSQLV4DataContext dc = new TSQLV4DataContext();
+            daneZTabeli(dc);
+        }
+        static void daneZTabeli(TSQLV4DataContext dc)
+        {
+            var data = from p in dc.Product
+                       select p;
+            var firstFive = data.Take(5);
+            foreach (var item in firstFive)
+            {
+                Console.WriteLine("Id: {0}, Numer: {1}, Nazwa: {2}", item.ProductID, item.ProductNumber, item.Name);
+            }
+            Console.ReadKey();
         }
     }
 }
